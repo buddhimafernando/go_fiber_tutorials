600c9415811049d79242cc217d003dce68ce29c6
Buddhima Fernando <harun.fernando.hf@gmail.com>
2024-06-03 22:51:49 +0530

    chore: Add go.mod and go.sum files with updated dependencies

--- main.go
+++ main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+	//"github.com/gofiber/fiber/v2"
 )
+
+type Server struct{
+	http.Handler
+
+	Address 			string
+	DocumentRoot 		string
+	DirectoryListing 	bool
+	DirectoryIndex 		[]string
+	AliasMap 			map[string]string
+	
+}
+
+
+
+// func FileServer() *Server {
+// }
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	dir, _ :=os.Getwd()
+
+	//app := fiber.New()
+
+	http.ListenAndServe(":3000",http.FileServer(http.Dir(dir)))
 }
