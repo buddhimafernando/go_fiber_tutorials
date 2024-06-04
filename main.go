3f268fbe82283f06803b85756f0b3b666ff66fbf
Buddhima Fernando <harun.fernando.hf@gmail.com>
2024-06-04 12:03:05 +0530

    The code changes include adding a file server and download functionality

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
-	//"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 type Server struct{
@@ -18,17 +20,39 @@ type Server struct{
 	
 }
 
-
-
-// func FileServer() *Server {
-// }
+// creating a nes file server structure
+func FileServer() *Server {
+	server:= &Server{
+		Address: ":3000",
+		DocumentRoot: "./public", // current directory
+		DirectoryListing: false,
+		DirectoryIndex: []string{"index.html"},
+		AliasMap: map[string]string{},
+	}
+	return server
+}
 
 func main() {
 	fmt.Println("Hello, World!")
 
-	dir, _ :=os.Getwd()
+	app := fiber.New()
 
-	//app := fiber.New()
+	// use logger middleware
+	app.Use(logger.New())
 
-	http.ListenAndServe(":3000",http.FileServer(http.Dir(dir)))
+	// serve static files from the public directory
+	app.Static("/files","./public")
+
+	// route to download specific files
+	app.Get("/download/:file", func(c *fiber.Ctx) error {
+		fileName := c.Params("file")
+		filePath := "./public/" + fileName
+		if _, err := os.Stat(filePath); os.IsNotExist(err) {
+			return c.Status(fiber.StatusNotFound).SendString("File not found")
+		}
+		return c.Download(filePath)
+	})
+
+
+	log.Fatal(app.Listen(":3000"))
 }
